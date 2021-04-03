1eb3d355e8b2706c6142bcffb15cd1ed4805b750
tiborkoderman <tibor.koderman@gmail.com>
2021-04-03 02:48:01 +0200

    todo: mosquitto connection

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ con.connect(function(err) {
 const ws = new WebSocket.Server({port:8088});
 
 ws.on('connection', function connection(wsConnection) {
-    var userID;
-    wsConnection.on('message', function incoming(message) {
+    var userID = null;
+    wsConnection.on('message', async function incoming(message) {
     //console.log(`server received: ${message}`);
     
     let messObj = JSON.parse(message)
@@ -27,37 +27,51 @@ ws.on('connection', function connection(wsConnection) {
 
     if(messObj.objType === `loginInfo`)
     {
-    //console.log(user);
-    let sql = "SELECT * FROM user WHERE username=? AND password = ?";
-    let inserts = [messObj.username, messObj.password];
-    sql = mysql.format(sql,inserts);
-   // console.log(sql);
-    con.query(sql, function(err,result,fields){
-        if (err) throw err;
-        console.log(result);
+        //console.log(user);
+        let sql = "SELECT * FROM user WHERE username=? AND password = ?";
+        let inserts = [messObj.username, messObj.password];
+        sql = mysql.format(sql,inserts);
+    // console.log(sql);
+        con.query(sql, function (err,result,fields){
+            if (err) 
+                throw err;
+            console.log(result);
 
-        console.log(userID);
-        if(result.length == 0)
+            if(result[0].username === messObj.username && result[0].password === messObj.password)
+            {
+                console.log(`[\x1b[32mOK\x1b[37m] Login success, from: ${wsConnection._socket.remoteAddress}`)
+                wsConnection.send(`Login success`)
+                userID = result[0].id;
+                console.log(userID);
+            }
+            else if (result.length == 0)
             {
                 console.log(`[\x1b[31mWARN\x1b[37m] Username or password is incorrect, attempted from: ${wsConnection._socket.remoteAddress}`);
                 wsConnection.send(`Username or password is incorrect`);
                 //wsConnection.close();
-                userID = result[0].id;
             }
-        else
+
+        console.log(`userID: ${userID}`);
+        if(userID !== undefined && userID !== null)
         {
-            console.log(`[\x1b[32mOK\x1b[37m] Login success, from: ${wsConnection._socket.remoteAddress}`)
-            wsConnection.send(`Login success`)
+            sql = `SELECT un.mac, un.type FROM user us JOIN unit un ON (un.userId=us.id) WHERE us.id = ?`;
+            let sql2 = `SELECT un.mac, un.type FROM user us JOIN unit un ON (un.userId=us.id) WHERE us.id = 1`;
+            inserts = [userID];
+            console.log(`user id: ${userID}`);
+            sql = mysql.format(sql,inserts);
+            console.log(sql);
+            con.query(sql,function(err,result,fields){
+                if (err) {console.log(err); /*throw err*/};
+                console.log(result);
+                let data = {
+                    type: "deviceList",
+                    data: result,
+                }
+                wsConnection.send(JSON.stringify(data));
+                });
         }
         });
-
-    sql = `SELECT un.mac, un.type FROM user us JOIN unit un ON (un.userId=us.id) WHERE us.id = 1`;
-    console.log(sql);
-    con.query(sql, function(err,result,fields){
-        if (err) {console.log(err); /*throw err*/};
-        console.log(result);
-        //ws.send(JSON.stringify(result));
-        });
+        
     }
     else if(messObj.objType===`debugMessage`)
         console.log(messObj.message);
