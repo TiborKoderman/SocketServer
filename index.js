ba880ea1fad156b38d3200b71f60e5553775af6d
tiborkoderman <tibor.koderman@gmail.com>
2021-05-09 09:47:44 +0200

    basic functionality working

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql')
 const WebSocket = require('ws');
-var mqtt = require('mqtt');
-var mqttcli = mqtt.connect('koderman.net:1883')
+var mqttlib = require('mqtt');
+
 
 var con = mysql.createConnection({
     host: "localhost",
@@ -19,12 +19,11 @@ const ws = new WebSocket.Server({port:8088});
 
 ws.on('connection', function connection(wsConnection) {
     var userID = null;
-    wsConnection.on('message', async function incoming(message) {
+    wsConnection.on('message', function incoming(message) {
     //console.log(`server received: ${message}`);
-    
+    let mqtt = mqttlib.connect('mqtt:koderman.net');
     let messObj = JSON.parse(message)
-
-
+    
     if(messObj.objType === `loginInfo`)
     {
         //console.log(user);
@@ -37,25 +36,25 @@ ws.on('connection', function connection(wsConnection) {
                 throw err;
             console.log(result);
 
-            if(result[0].username === messObj.username && result[0].password === messObj.password)
+            if (result.length == 0)
+            {
+                console.log(`[\x1b[31mWARN\x1b[37m] Username or password is incorrect, attempted from: ${wsConnection._socket.remoteAddress}`);
+                wsConnection.send(`Username or password is incorrect`);
+                //wsConnection.close();
+            }
+            else if(result[0].username === messObj.username && result[0].password === messObj.password)
             {
                 console.log(`[\x1b[32mOK\x1b[37m] Login success, from: ${wsConnection._socket.remoteAddress}`)
                 wsConnection.send(`Login success`)
                 userID = result[0].id;
                 console.log(userID);
             }
-            else if (result.length == 0)
-            {
-                console.log(`[\x1b[31mWARN\x1b[37m] Username or password is incorrect, attempted from: ${wsConnection._socket.remoteAddress}`);
-                wsConnection.send(`Username or password is incorrect`);
-                //wsConnection.close();
-            }
+            
 
-        console.log(`userID: ${userID}`);
+        //console.log(`userID: ${userID}`);
         if(userID !== undefined && userID !== null)
         {
             sql = `SELECT un.mac, un.type FROM user us JOIN unit un ON (un.userId=us.id) WHERE us.id = ?`;
-            let sql2 = `SELECT un.mac, un.type FROM user us JOIN unit un ON (un.userId=us.id) WHERE us.id = 1`;
             inserts = [userID];
             console.log(`user id: ${userID}`);
             sql = mysql.format(sql,inserts);
@@ -68,6 +67,10 @@ ws.on('connection', function connection(wsConnection) {
                     data: result,
                 }
                 wsConnection.send(JSON.stringify(data));
+                    result.forEach(e => {
+                        console.log(`subscribed to: ${e.mac}`);
+                        mqtt.subscribe(e.mac);
+                    });
                 });
         }
         });
@@ -75,13 +78,21 @@ ws.on('connection', function connection(wsConnection) {
     }
     else if(messObj.objType===`debugMessage`)
         console.log(messObj.message);
-    else if(messObj.objType===`mqttReq`)
+    else if(messObj.objType===`stc`)
     {
-        // Handle mqtt stuff
+        //console.log(messObj.mac+" "+messObj.allData);
+        mqtt.publish(messObj.mac, JSON.stringify(messObj.allData));
     }
-
-   
-    //let sql:
+    mqtt.on('message',function(topic, message, packet){
+        console.log("message is "+ message);
+        //console.log("topic is "+ topic);
+        try{
+            message = JSON.parse(message);
+            if(message.objType==="cts")
+                wsConnection.send(JSON.stringify(message));
+        }catch{console.log("Wrong json format");}
+    });
+    
 });
     
 
